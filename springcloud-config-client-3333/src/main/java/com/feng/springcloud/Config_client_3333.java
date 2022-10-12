4ec2910ad76cc73dd3603626cdd6c9ebed9ddeb2
waitwaitww <yf20010814@outlook.com>
2022-10-12 23:07:32 +0800

    1.0

--- springcloud-config-client-3333/src/main/java/com/feng/springcloud/Config_client_3333.java
+++ springcloud-config-client-3333/src/main/java/com/feng/springcloud/Config_client_3333.java
@@ -1,11 +1,11 @@
 package com.feng.springcloud;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Config_client_3333 {
-
     public static void main(String[] args) {
-
+        SpringApplication.run(Config_client_3333.class,args);
     }
 }
